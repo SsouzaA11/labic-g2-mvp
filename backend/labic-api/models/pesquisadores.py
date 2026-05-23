cd7338d971c6d4db3355d6eee82f49bd48d32eea
Igo <igofer1435@gmail.com>
2026-05-22 23:06:36 -0300

    fix: resolve o erro de import do sqlalchemy e corrigido algumas coluna de acordo com o der

--- backend/labic-api/models/pesquisadores.py
+++ backend/labic-api/models/pesquisadores.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class PesquisadorModel(Base):
     __tablename__ = "pesquisadores"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id_pesquisador = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nome = Column(String(150), nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
-    senha = Column(String(255), nullable=False)
-    biografia = Column(Text, nullable=True)
-    link_lattes = Column(String(255), nullable=True)
+    senha_hash = Column(String(255), nullable=False)
+    titulacao = Column(String(100), nullable=True)
+    tipo_vinculo = Column(String(100), nullable=True)
+    instituicao = Column(String(150), nullable=True)
+    is_admin = Column(Boolean, default=False)
